fee517b96a73dc31356fe25c6b45f16eb2ada99e
marshl <marshl@live.com.au>
2019-11-29 22:40:34 +1100

    Reversed the traffic flow

--- magick_traffic/main.cpp
+++ magick_traffic/main.cpp
@@ -13,26 +13,26 @@ int main( int argc, char* argv[] )
     const int height = 512;
 
     const int frameCount = 128;// 512;
-    const int holdingFrames = 16;
+    //const int holdingFrames = 16;
 
     system( "rd /s /q output" );
     system( "md output" );
 
-    Image originalImage;
-    originalImage.read( "Animated_lights_on_the_Christmas_tree_part_1-512px.png" );
+    //Image originalImage;
+    //originalImage.read( "Animated_lights_on_the_Christmas_tree_part_1-512px.png" );
 
-    TrafficAutomata tf( originalImage.columns(), originalImage.rows() );
-    //TrafficAutomata tf( width, height );
-    //tf.Randomise( 0.35f );
-    for ( int y = 0; y < originalImage.rows(); ++y )
+    //TrafficAutomata tf( originalImage.columns(), originalImage.rows() );
+    TrafficAutomata tf( width, height );
+    tf.Randomise( 0.35f );
+    /*for ( int y = 0; y < height; ++y )
     {
-        for ( int x = 0; x < originalImage.columns(); ++x )
+        for ( int x = 0; x < width; ++x )
         {
             TrafficCell* cell = tf.GetCell( x, y );
             Color colour = originalImage.pixelColor( x, y );
             cell->direction = colour.alphaQuantum() != 0 ? UNOCCUPIED : ( rand() % 2 ? VERTICAL : HORIZONTAL );
         }
-    }
+    }*/
 
     std::vector<Image> frameList;
     for ( int i = 0; i < frameCount; ++i )
@@ -44,8 +44,8 @@ int main( int argc, char* argv[] )
             {
                 TrafficCell* cell = tf.GetCell( x, y );
 
-                //Color color( cell->direction != UNOCCUPIED ? ( cell->direction == HORIZONTAL ? "red" : "green" ) : "black" );
-                Color c;
+                Color color( cell->direction != UNOCCUPIED ? ( cell->direction == HORIZONTAL ? "red" : "green" ) : "black" );
+                /*Color c;
                 if ( cell->direction == UNOCCUPIED )
                 {
                     c = Color( "white" );
@@ -54,18 +54,18 @@ int main( int argc, char* argv[] )
                 {
                     //c = Color( ( (float)cell->originalX / (float)width ) * QuantumRange, ( (float)cell->originalY / (float)height ) * QuantumRange, 0 );
                     c = originalImage.pixelColor( cell->originalX, cell->originalY );
-                }
-                blank_image.pixelColor( x, y, c );
+                }*/
+                blank_image.pixelColor( x, y, color );
             }
         }
 
-        if ( i > holdingFrames )
+        //if ( i > holdingFrames )
         {
             tf.Multistep( 50 );
         }
 
         char filename[255];
-        sprintf_s( filename, 255, "output/frame%03d.png", frameCount - i );
+        sprintf_s( filename, 255, "output/frame%03d.png", i );// frameCount - i );
         //blank_image.magick( "png" );
         blank_image.write( filename );
         //frameList.push_back( blank_image );
